c492b2629abf111404debd2d977a8d1c7e50fa5c
Akuna444 <aklilsolomon3@gmail.com>
2024-07-22 22:19:48 +0300

    add: all filter option

--- client/song-player/src/app/page.tsx
+++ client/song-player/src/app/page.tsx
@@ -28,7 +28,12 @@ export default function Home() {
                 Filter by genre
               </label>
               <select
-                onChange={(e) => setGenre(e.target.value)}
+                onChange={(e) => {
+                  if (e.target.value === "All") {
+                    return setGenre("");
+                  }
+                  setGenre(e.target.value);
+                }}
                 id="countries"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
