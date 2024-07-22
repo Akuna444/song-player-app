ab56b7c81321d4d0316755b04fa4e983288e49d4
Akuna444 <aklilsolomon3@gmail.com>
2024-07-22 16:00:29 +0300

    add: genres dropdown

--- client/song-player/src/app/create/page.tsx
+++ client/song-player/src/app/create/page.tsx
@@ -5,6 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useCreateSongMutation } from "@/lib/services/api";
 import { useRouter } from "next/navigation";
+import { genres } from "@/constants/genres";
 
 const songSchema = z.object({
   title: z.string().nonempty("Title is required"),
@@ -83,14 +84,19 @@ const AddSongs = () => {
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
           Genre
         </label>
-        <input
+        <select
           {...register("genre")}
-          type="text"
-          placeholder="Genre Name"
-          id="genre"
+          id="countries"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-        />
+        >
+          {genres.map((genre) => {
+            return (
+              <option key={genre} value={genre}>
+                {genre}
+              </option>
+            );
+          })}
+        </select>
         {errors.genre && <p className="text-red-500">{errors.genre.message}</p>}
       </div>
       <button
