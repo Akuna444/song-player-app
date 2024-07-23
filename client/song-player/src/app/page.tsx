5187e1780fd71107a139467d26acba73c1cb8c51
Akuna444 <aklilsolomon3@gmail.com>
2024-07-23 04:05:42 +0300

    fix:build errors

--- client/song-player/src/app/page.tsx
+++ client/song-player/src/app/page.tsx
@@ -24,54 +24,54 @@ export default function Home() {
     toast.error("Failed to fetch");
   }
   return (
-    <Suspense>
-      <div className="section-padding">
-        {isLoading ? (
-          <HomeSkeleton />
-        ) : (
-          <>
-            <Stats />
-            <div className="w-full flex justify-center mb-10">
-              <Button link="statistics" title="More Stats" />
-            </div>
+    <div className="section-padding">
+      {isLoading ? (
+        <HomeSkeleton />
+      ) : (
+        <>
+          <Stats />
+          <div className="w-full flex justify-center mb-10">
+            <Button link="statistics" title="More Stats" />
+          </div>
 
-            <div className="w-full flex-col flex md:flex-row gap-4  justify-between">
-              <div className="mb-5">
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Filter by genre
-                </label>
-                <select
-                  onChange={(e) => {
-                    if (e.target.value === "All") {
-                      return setGenre("");
-                    }
-                    setGenre(e.target.value);
-                  }}
-                  id="countries"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                >
-                  {genres.map((genre) => {
-                    return (
-                      <option key={genre} value={genre}>
-                        {genre}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>
-              <Search />
-              <Button link="create" title="Add New" />
+          <div className="w-full flex-col flex md:flex-row gap-4  justify-between">
+            <div className="mb-5">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Filter by genre
+              </label>
+              <select
+                onChange={(e) => {
+                  if (e.target.value === "All") {
+                    return setGenre("");
+                  }
+                  setGenre(e.target.value);
+                }}
+                id="countries"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                {genres.map((genre) => {
+                  return (
+                    <option key={genre} value={genre}>
+                      {genre}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
-            {data &&
-              (data.songs?.length === 0 ? (
-                <h3>No Songs Found</h3>
-              ) : (
-                <Table count={data?.count} songs={data?.songs} />
-              ))}
-          </>
-        )}
-        {error && <p>Failed to fetch</p>}
-      </div>
-    </Suspense>
+            <Suspense>
+              <Search />
+            </Suspense>
+            <Button link="create" title="Add New" />
+          </div>
+          {data &&
+            (data.songs?.length === 0 ? (
+              <h3>No Songs Found</h3>
+            ) : (
+              <Table count={data?.count} songs={data?.songs} />
+            ))}
+        </>
+      )}
+      {error && <p>Failed to fetch</p>}
+    </div>
   );
 }
