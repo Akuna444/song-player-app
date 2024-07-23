017892eb2956883f9ba4ab4f4ed158ba58eab3d8
Akuna444 <aklilsolomon3@gmail.com>
2024-07-23 05:01:34 +0300

    add: more cors orgion

--- server/app.js
+++ server/app.js
@@ -8,10 +8,21 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://walia-song-player-app.vercel.app",
+];
+
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests from this origin
-  methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-  allowedHeaders: "Content-Type", // Allowed headers
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type",
 };
 
 // Enable CORS
